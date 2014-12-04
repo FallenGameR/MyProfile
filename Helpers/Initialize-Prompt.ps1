8b847daf68c1ac1fcb95ca9213a241c4b2144acd
Alexander Kostikov <alexko@microsoft.com>
2014-12-04 14:06:39 -0800

    profile refactoring

---
+++ Helpers/Initialize-Prompt.ps1
@@ -0,0 +1,37 @@
+# History folder and file
+$SCRIPT:historyFolder = "c:\automation\history\"
+if( -not (Test-Path $historyFolder) )
+{
+    mkdir $historyFolder -ea Ignore | Out-Null
+}
+$SCRIPT:historyFile = "$historyFolder\{0}--$pid.ps1" -f [DateTime]::Now.ToString("yyyy.MM.dd--HH.mm.ss--UTCz")
+$SCRIPT:lastCommandId = -1
+
+# Prompt
+$function:prompt = {
+    $realLastExitCode = $LASTEXITCODE
+    Set-ConsoleFont 10
+
+    # Preserve last command in log file
+    $lastCommand = history -Count 1
+    if( $lastCommand )
+    {
+        if( $lastCommand.Id -ne $SCRIPT:lastCommandId )
+        {
+            $SCRIPT:lastCommandId = $lastCommand.Id
+            Add-Content $historyFile $lastCommand.CommandLine
+        }
+    }
+    else
+    {
+        $lastCommandId = -1
+        Add-Content $historyFile "# No history at this point of time"
+    }
+
+    # Update prompt
+    $host.UI.RawUI.WindowTitle = "$pwd   [$Env:ComputerName]   $env:UserDomain\$env:UserName"
+    Write-Host "$pwd" -ForegroundColor DarkYellow -NoNewline
+    Write-Host " [$Env:ComputerName] $env:UserDomain\$env:UserName" -ForegroundColor DarkGreen
+    $LASTEXITCODE = $realLastExitCode
+    [char] 187 + " "
+}
--- profile.ps1
+++ profile.ps1
@@ -1,31 +1,31 @@
-# Global variables
+# Powershell behaviour setup
 $global:Profile = $PSCommandPath
 $global:MaximumHistoryCount = 1024
 
-# Include dependencies
-. $PSScriptRoot\Helpers\Set-ConsoleFont.ps1 | Out-Null
-. $PSScriptRoot\Helpers\Playground.ps1
-
-# CoreXTAutomation with codeflow pointing to dogfood version
-Import-Module CoreXtAutomation -DisableNameChecking
-${GLOBAL:CoreXTAutomation.CodeFlow} = "\\codeflow\public\cfdog.cmd"
-
-# Show all matching commands by default
 $PSDefaultParameterValues["Get-Command:All"] = $true
 $PSDefaultParameterValues["Set-Content:Encoding"] = "ASCII"
 $PSDefaultParameterValues["edit:NewEditor"] = $true
 
-# Use custom formatting for gcm command
 Update-FormatData -PrependPath "$PSScriptRoot\Format.Custom.ps1xml"
 
-# Includes
-$env:Path += ";c:\tools\BeyondCompare4\"
-$env:Path += ";c:\tools\SysinternalsSuite\"
-$env:Path += ";c:\tools\Vim\vim74\"
-$env:Path += ";c:\tools\ILSpy\"
-$env:Path += ";c:\tools\LinqPad\"
-Import-Module PsReadLine
+New-Alias new New-Object
+New-Alias rename Rename-Item
+New-Alias m measure
+
+# Environment setup
+$addToPath =
+    "c:\tools\BeyondCompare4\",
+    "c:\tools\SysinternalsSuite\",
+    "c:\tools\Vim\vim74\",
+    "c:\tools\ILSpy\",
+    "c:\tools\LinqPad\"
+
+$env:Path += ";" + ($addToPath -join ";")
+$env:SdEditor = 'C:\tools\Vim\vim73\gvim.exe'
+
 #Set-StrictMode -Off
+
+Import-Module PsReadLine
 
 # Setting up PSReadLine
 $colors =
@@ -44,49 +44,10 @@ $colors =
 foreach( $token in $colors.Keys ) { Set-PSReadlineOption -ForegroundColor ($colors.$token) -TokenKind $token }
 Set-PSReadlineKeyHandler -Chord "Ctrl+D,Ctrl+C" -Function CaptureScreen
 
-# For SD
-$env:sdeditor = 'C:\tools\Vim\vim73\gvim.exe'
+# CoreXTAutomation setup
+${GLOBAL:CoreXTAutomation.CodeFlow} = "\\codeflow\public\cfdog.cmd"
 
-# Aliases
-New-Alias new New-Object
-New-Alias rename Rename-Item
-New-Alias m measure
-
-# History folder and file
-$SCRIPT:history = "c:\automation\history\"
-if( -not (Test-Path $history) )
-{
-    mkdir $history 2>&1 | Out-Null
-}
-$timeStamp = [DateTime]::Now.ToString("yyyy.MM.dd--HH.mm.ss--UTCz")
-$SCRIPT:historyFile = "$history\$timestamp--$pid.ps1"
-$SCRIPT:lastCommandId = -1
-
-# Prompt
-$function:prompt = {
-    $realLastExitCode = $LASTEXITCODE
-    Set-ConsoleFont 10
-
-    # Preserve last command in log file
-    $lastCommand = history -Count 1
-    if( $lastCommand )
-    {
-        if( $lastCommand.Id -ne $SCRIPT:lastCommandId )
-        {
-            $SCRIPT:lastCommandId = $lastCommand.Id
-            Add-Content $historyFile $lastCommand.CommandLine
-        }
-    }
-    else
-    {
-        $lastCommandId = -1
-        Add-Content $historyFile "# No history at this point of time"
-    }
-
-    # Update prompt
-    $host.UI.RawUI.WindowTitle = "$pwd   [$Env:ComputerName]   $env:UserDomain\$env:UserName"
-    Write-Host "$pwd" -ForegroundColor DarkYellow -NoNewline
-    Write-Host " [$Env:ComputerName] $env:UserDomain\$env:UserName" -ForegroundColor DarkGreen
-    $LASTEXITCODE = $realLastExitCode
-    [char] 187 + " "
-}
+# Additional setup
+. $PSScriptRoot\Helpers\Playground.ps1
+. $PSScriptRoot\Helpers\Set-ConsoleFont.ps1 | Out-Null
+. $PSScriptRoot\Helpers\Initialize-Prompt.ps1
