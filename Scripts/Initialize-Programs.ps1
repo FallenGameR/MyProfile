64dcd9d48974c4d5a70306ade2f3579bbf0356e9
Alexander Kostikov <Alex.Kostikov@gmail.com>
2015-08-03 18:10:13 -0700

    added quick note about win10 package management

--- Scripts/Initialize-Programs.ps1
+++ Scripts/Initialize-Programs.ps1
@@ -1,5 +1,12 @@
 #Requires -RunAsAdministrator
 
+
+Find-Package ack -Provider chocolatey | Install-Package -Force -ForceBootstrap -Verbose
+
+Install-package –ProviderName chocolatey –Force –FirceBootStrap –Verbose –Name <package>
+
+
+
 Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install git -y
 choco install gitextensions -y
