6f505309c7b342f7c48bff77a7ca0a833ab54ae2
Alexander Kostikov <alexko@microsoft.com>
2020-05-06 14:39:21 -0700

    Cleaning up

--- Scripts/Exported/ConvertFrom-SecureStringAsPlainText.ps1
+++
@@ -1,5 +0,0 @@
-param([System.Security.SecureString] $secureString = $(throw "Please specifiy a SecureString"))
-
-$bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureString)
-[System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
-[System.Runtime.InteropServices.Marshal]::ZeroFreeBstr($bstr)
