dbcbb79f932d8ba50707be5fb330e56ea033e1af
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2020-10-27 11:43:18 -0700

    Update Setup-Machine.ps1

--- Provision/Setup-Machine.ps1
+++ Provision/Setup-Machine.ps1
@@ -8,25 +8,25 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 # C:\ProgramData\chocolatey\lib, C:\src as well
 
 # Tools instalation
-choco install powershell-core googlechrome firacode less microsoft-windows-terminal vscode git gitextensions kdiff3 windirstat beyondcompare gimp ilspy sysinternals mp3tag far mls-software-openssh -y
+choco install -s=chocolatey powershell-core googlechrome firacode less microsoft-windows-terminal vscode git gitextensions kdiff3 windirstat beyondcompare gimp ilspy sysinternals mp3tag far mls-software-openssh -y
 
 # linqpad5 CRC broken, find alternatives, for now we are taking it from OneDrive and MyProfile
 #choco install linqpad5 -y
 
 # Visual studio
-choco install visualstudio2019enterprise visualstudio2019-workload-azure visualstudio2019-workload-manageddesktop visualstudio2019-workload-nativedesktop visualstudio2019-workload-netcoretools -y
+choco install -s=chocolatey visualstudio2019enterprise visualstudio2019-workload-azure visualstudio2019-workload-manageddesktop visualstudio2019-workload-nativedesktop visualstudio2019-workload-netcoretools -y
 
 
 # MS specific
 if( $MsSpecific )
 {
-    choco install microsoft-teams -y
+    choco install -s=chocolatey microsoft-teams -y
 }
 
 # Home specific
 if( $HomeSpecific )
 {
-    choco install -y steam geforce-game-ready-driver
+    choco install -s=chocolatey -y steam geforce-game-ready-driver
     # OneDrive setup - unlink and relink to change folder
 }
 
