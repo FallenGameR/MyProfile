25fbf55f3591efa61ddaa9ea97966d0eb416dc62
Aleksandr Kostikov <alexko@microsoft.com>
2022-08-03 15:27:14 -0700

    more tweaks

--- profile.ps1
+++ profile.ps1
@@ -52,7 +52,7 @@ $env:LESS = "-IeFRX"
 $env:BAT_THEME = "Visual Studio Dark+"
 $env:RIPGREP_CONFIG_PATH = "$PSScriptRoot\rg.config"
 #$env:DELTA_PAGER = "0" # By default it uses less with -R incompatible switch set - but this breaks git
-$env:FZF_DEFAULT_OPTS = "--layout=reverse --height 60% --info=hidden --tabstop=4"
+$env:FZF_DEFAULT_OPTS = "--layout=reverse --height 60% --info=hidden --tabstop=4 -m --cycle"
 
 tm environment
 
