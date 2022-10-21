f08fff7b812762074f8adeb981a58ce6666c03db
Alexander Kostikov <alexko@microsoft.com>
2022-10-21 16:29:43 -0700

    sd default editor added

--- Scripts/Initialize-Environment.ps1
+++ Scripts/Initialize-Environment.ps1
@@ -6,6 +6,7 @@ $addToPath =
     "C:\tools\chafa"
 $env:Path += ";" + ($addToPath -join ";")
 
+$env:SDFORMEDITOR="C:\Program Files\Microsoft VS Code\code.exe"
 $env:LESS = "-IeFRX"
 $env:RIPGREP_CONFIG_PATH = "$PSScriptRoot\..\rg.config"
 $env:BAT_CONFIG_PATH = "$PSScriptRoot\..\bat.config"
