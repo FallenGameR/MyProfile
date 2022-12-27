7fbeff302743a9135261ee4466ed4aad0806e08a
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2022-12-26 22:44:52 -0800

    folders are less striking

--- Scripts/Initialize-Fzf.ps1
+++ Scripts/Initialize-Fzf.ps1
@@ -316,16 +316,4 @@ function hf
     }
 }
 
-function capp( $url, [switch] $Music )
-{
-    if( $music )
-    {
-        & "C:\Program Files\Google\Chrome\Application\chrome.exe" "--app=https://music.youtube.com"
-    }
-    else
-    {
-        & "C:\Program Files\Google\Chrome\Application\chrome.exe" "--app=$url"
-    }
-}
-
 tm (Split-Path $PSCommandPath -Leaf)
