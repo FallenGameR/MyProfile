f2c8faee8124222ca3e5dfbe6aa35de7b7045ffb
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2022-12-26 22:22:26 -0800

    Provisioning script refining

---
+++ Scripts/Initialize-WindowsApps.ps1
@@ -0,0 +1,26 @@
+
+Complete-Once "Windows apps" {
+    $apps = cat "$PSScriptRoot/windows-apps.txt"
+    choco install -s=chocolatey @apps -y
+}
+
+Complete-Once "Mouse setup" {
+    & "$env:OneDriveConsumer\Distrib\Hardware\Deft Pro Trackball\mouse_driver_ma5111000.exe"
+}
+
+Complete-Once "Tools folder" {
+    if( -not (Test-Path "c:\tools") )
+    {
+        mkdir "c:\tools" -ea Stop | Out-Null
+    }
+
+    foreach( $tool in ls $env:OneDriveConsumer\Apps\tools -Directory -ea Ignore | where Name -notmatch "^_" )
+    {
+        New-Junction $tool.FullName "c:\tools\$($tool.Name)"
+    }
+
+    foreach( $tool in ls $env:OneDriveCommercial\tools -Directory -ea Ignore | where Name -notmatch "^_" )
+    {
+        New-Junction $tool.FullName "c:\tools\$($tool.Name)"
+    }
+}
