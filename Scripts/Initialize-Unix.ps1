7216dc0ad3c9b2fec0aac61e0e1f3f7b62a494b9
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2022-12-27 14:21:02 -0800

    bat for man

--- Scripts/Initialize-Unix.ps1
+++ Scripts/Initialize-Unix.ps1
@@ -2,6 +2,9 @@
 function bat { batcat @args }
 function mkdir { New-Item -ItemType Directory @args }
 
+# Use bat for man
+$env:MANPAGER = "sh -c 'col -bx | batcat -l man -p'"
+
 # Default conhost console color setup
 Complete-Once "Gnome terminal colors" {
     Push-Location "$PsScriptRoot/../Bin/ColorTool/"
