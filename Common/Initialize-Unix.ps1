ed00f26635405eb3bfbb0d29f2cc001ea2b8df3a
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2022-12-26 20:22:10 -0800

    fix for gnome colors

--- Common/Initialize-Unix.ps1
+++ Common/Initialize-Unix.ps1
@@ -1,3 +1,4 @@
+# Common command names
 function bat
 {
     batcat @args
@@ -6,6 +7,13 @@ function bat
 function mkdir
 {
     New-Item -ItemType Directory @args
+}
+
+# Default conhost console color setup
+Complete-Once "Gnome terminal colors" {
+    Push-Location "$PsScriptRoot/../Bin/ColorTool/"
+    cat ./campbell.gnome_terminal | dconf load /org/gnome/terminal/
+    Pop-Location
 }
 
 tm (Split-Path $PSCommandPath -Leaf)
