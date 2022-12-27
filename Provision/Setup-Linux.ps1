4726e042718295c911be2f56ee2ce752551ffc5b
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2022-12-26 16:07:23 -0800

    ctrl+x working

--- Provision/Setup-Linux.ps1
+++ Provision/Setup-Linux.ps1
@@ -1,8 +1,20 @@
 
-
-# Install
-
+```ps1
+# SpellChecker: disable
 xsel - clipboard manager
 git
 git credential manager + pgp, pass, setup
 vscode
+kdiff3
+
+## SmartGit
+tar xzf ./smartgit-linux-22_1_2.tar.gz
+cd smart*
+bin/smartgit.sh
+bin/add-menuitem.sh
+
+
+
+git config --global core.editor "code"
+```
+
