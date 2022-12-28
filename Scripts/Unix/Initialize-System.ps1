88214c7f38f57a7c72836bb0cf13f46adf2772b7
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2022-12-27 22:47:24 -0800

    open analog

--- Scripts/Unix/Initialize-System.ps1
+++ Scripts/Unix/Initialize-System.ps1
@@ -5,6 +5,7 @@ $env:PATH += [io.path]::PathSeparator + (($addToPath | where{ Test-Path $psitem 
 
 # Common command names
 function mkdir { New-Item -ItemType Directory @args }
+function open { bash -c "doublecmd $pwd &>/dev/null & disown" }
 
 # Use bat for man
 $env:MANPAGER = "sh -c 'col -bx | batcat -l man -p'"
