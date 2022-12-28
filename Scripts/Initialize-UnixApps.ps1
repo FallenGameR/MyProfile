d3bbd492c5ff787e84b624a03221ae043c7d2b55
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2022-12-27 17:32:21 -0800

    glow unix install instructions

--- Scripts/Initialize-UnixApps.ps1
+++ Scripts/Initialize-UnixApps.ps1
@@ -3,4 +3,13 @@ Complete-Once "Unix apps" {
     sudo apt install @apps -y
 }
 
+<#
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow
+
+#>
+
 tm (Split-Path $PSCommandPath -Leaf)
