553d56f58d4de17675f34d083efa4f42b8c4754b
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-01-20 12:49:58 -0800

    fzf defaults are needed in profile

--- Scripts/Initialize-Fzf.ps1
+++ Scripts/Initialize-Fzf.ps1
@@ -1,3 +1,44 @@
+# Default FZF options
+$fzfOptions = @(
+    "--layout=reverse",             # Grow list down, not upwards
+    "--tabstop=4",                  # Standard tab size
+    "--multi",                      # Multi select possible
+    "--bind",                       # Shortcuts:
+    "alt-t:toggle-all",             # Alt+t toggles selection
+    "--cycle",                      # Cycle the list
+    "--ansi",                       # Use Powershell colors
+    "--no-mouse",                   # We need terminal mouse behavior, not custom one
+    "--tiebreak='length,index'",    # Priorities to resolve ties (index comes last always)
+    "--color=bg:#0C0C0C",           # Background (current line) = Black
+    "--color=bg+:#0C0C0C",          # Background (current line) = Black
+    "--color=fg+:#F2F2F2",          # Text (current line) = White
+    "--color=hl+:#13A10E",          # Highlighted substrings (current line) = DarkGreen
+    "--color=pointer:#3A96DD",      # Pointer to the current line = DarkCyan
+    "--color=preview-bg:#0C0C0C",   # Preview window background = Black
+    "--color=prompt:#CCCCCC"        # Prompt = Gray
+
+    # Argument help
+    #"--height 60%"                 # Leave some space intact - breaks cyrillic typing, bug created
+    #"--color=bg:#RRGGBB",          # Background
+    #"--color=bg+:#RRGGBB",         # Background (current line)
+    #"--color=border:#RRGGBB",      # Border of the preview window and horizontal separators (--border)
+    #"--color=fg:#RRGGBB",          # Text
+    #"--color=fg+:#RRGGBB",         # Text (current line)
+    #"--color=gutter:#RRGGBB",      # Gutter on the left (defaults to bg+)
+    #"--color=header:#RRGGBB",      # Header
+    #"--color=hl:#RRGGBB",          # Highlighted substrings
+    #"--color=hl+:#RRGGBB",         # Highlighted substrings (current line)
+    #"--color=info:#RRGGBB",        # Info
+    #"--color=marker:#RRGGBB",      # Multi-select marker
+    #"--color=pointer:#RRGGBB",     # Pointer to the current line
+    #"--color=preview-bg:#RRGGBB",  # Preview window background
+    #"--color=preview-fg:#RRGGBB",  # Preview window text
+    #"--color=prompt:#RRGGBB",      # Prompt
+    #"--color=spinner:#RRGGBB",     # Streaming input indicator
+)
+$env:FZF_DEFAULT_OPTS = $fzfOptions -join " "
+
+# Shortcuts
 Register-Shortcut "Alt+h" "hf" "History search"
 Register-Shortcut "Alt+o" "startf" "Open file"
 Register-Shortcut "Alt+r" "rgf" "Ripgrep search"
