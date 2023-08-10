4814d735bdd886cf1160dfede7c170224345666b
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-08-10 08:58:32 -0700

    oython installation

--- Scripts/Unix/Initialize-System.ps1
+++ Scripts/Unix/Initialize-System.ps1
@@ -11,10 +11,5 @@ function open { bash -c "doublecmd $pwd &>/dev/null & disown" }
 # Use bat for man
 $env:MANPAGER = "sh -c 'col -bx | batcat -l man -p'"
 
-# Default conhost console color setup
-Complete-Once "Gnome terminal colors" {
-    cd "$PsScriptRoot/../../Bin/ColorTool/"
-    cat ./campbell.gnome_terminal | dconf load /org/gnome/terminal/
-}
 
 tm (Split-Path $PSCommandPath -Leaf)
