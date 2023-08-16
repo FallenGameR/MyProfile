3069cb811c076a825a3f34430761e689d78edd4e
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-08-16 16:24:28 -0700

    linux new convention for setup names

--- Scripts/Unix/Initialize-Apps.ps1
+++ Scripts/Unix/Initialize-Apps.ps1
@@ -4,58 +4,56 @@
 
 # Spell-checker: disable
 
-Complete-Once "Unix apps" {
+Complete-Once install-apps {
     $apps = cat "$PSScriptRoot/../../Data/unix-apps.txt"
     sudo apt install @apps -y
 }
 
-Complete-Once "Fzf git install" {
+Complete-Once install-fzf {
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
 }
 
-Complete-Once "Git-fuzzy git install" {
+Complete-Once setup-fzf {
+    cd /usr/bin
+    sudo ln -s ~/.fzf/bin/fzf fzf
+    sudo ln -s ~/.fzf/bin/fzf-tmux fzf-tmux
+}
+
+Complete-Once install-git-fuzzy {
     git clone https://github.com/bigH/git-fuzzy.git ~/.git-fuzzy
     # It is added to PATH elsewhere
 }
 
-Complete-Once "Bat proper name" {
+Complete-Once setup-bat {
     cd /usr/bin
     sudo ln -s batcat bat
 }
 
-Complete-Once "Install chrome" {
+Complete-Once install-chrome {
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
     sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
     sudo apt update
     sudo apt install google-chrome-stable
 }
 
-Complete-Once "Fzf link" {
-    cd /usr/bin
-    sudo ln -s ~/.fzf/bin/fzf fzf
-    sudo ln -s ~/.fzf/bin/fzf-tmux fzf-tmux
-}
-
-Complete-Once "Arduino fix" {
-    # see https://bugs.launchpad.net/ubuntu/+source/arduino/+bug/1916278
-    sudo apt install libserialport0 patchelf
-    sudo patchelf --add-needed /usr/lib/x86_64-linux-gnu/libserialport.so.0 /usr/lib/x86_64-linux-gnu/liblistSerialsj.so.1.4.0
-}
-
-Complete-Once "Install bottom" {
+Complete-Once install-bottom {
     curl -LO https://github.com/ClementTsang/bottom/releases/download/0.9.4/bottom_0.9.4_amd64.deb
     sudo dpkg -i bottom_0.9.4_amd64.deb
 }
 
-Complete-Once "Install rust" {
+Complete-Once setup-bottom {
+    Copy-Item $PSScriptRoot\..\..\bottom.toml ~/.config/bottom/bottom.toml
+}
+
+Complete-Once install-rust {
     sudo apt update
     sudo apt upgrade
     sudo apt install curl gcc make build-essential -y
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 
-Complete-Once "Rust arduino" {
+Complete-Once install-rust-arduino {
     # https://blog.logrocket.com/complete-guide-running-rust-arduino/
     rustup toolchain install nightly
     sudo apt install avr-libc gcc-avr pkg-config avrdude libudev-dev build-essential -y
@@ -63,21 +61,23 @@ Complete-Once "Rust arduino" {
     cargo install cargo-generate
 }
 
-Complete-Once "Gnome terminal colors" {
+Complete-Once bugfix-arduino {
+    # see https://bugs.launchpad.net/ubuntu/+source/arduino/+bug/1916278
+    sudo apt install libserialport0 patchelf
+    sudo patchelf --add-needed /usr/lib/x86_64-linux-gnu/libserialport.so.0 /usr/lib/x86_64-linux-gnu/liblistSerialsj.so.1.4.0
+}
+
+Complete-Once setup-gnome-terminal {
     cd "$PsScriptRoot/../../Bin/ColorTool/"
     cat ./campbell.gnome_terminal | dconf load /org/gnome/terminal/
 }
 
-Complete-Once "Bottom setup" {
-    Copy-Item $PSScriptRoot\..\..\bottom.toml ~/.config/bottom/bottom.toml
-}
-
-Complete-Once fd-renaming {
+Complete-Once setup-fd {
+    # Symlink
     ln -s $(which fdfind) ~/.local/bin/fd
     # $HOME/.local/bin needs to be in PATH
-}
 
-Complete-Once fd-excludes {
+    # Default excludes
     $path = "~/.config/fd"
     mkdir "$path\fd" -ea Ignore | Out-Null
     cat "$PsScriptRoot/../../Modules/FzfBindings/Data/excluded_folders" > $path/fd/ignore
