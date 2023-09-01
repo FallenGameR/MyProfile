695d5101b001b0d75c1aecf259c962b177f710a3
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-09-01 14:07:01 -0700

    fixing paths

--- profile.ps1
+++ profile.ps1
@@ -33,4 +33,4 @@ $env:Path = "$env:Path;C:\tools\dcm.explorer"
 
 $env:PSModulePath = "C:\tools\JitShell;$env:PSModulePath"
 $env:PSModulePath = "D:\src\mv\src\Client\NTP\scripts\modules;$env:PSModulePath"
-$env:PSModulePath = "C:\tools\DriScripts;$env:PSModulePath"
+$env:PSModulePath = "$env:PSModulePath;C:\tools\DriScripts"
