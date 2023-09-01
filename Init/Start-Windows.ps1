3c5d36662cfd9bd98bf0b3c1619a4fc9edd8bc46
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-09-01 10:08:15 -0700

    Rust is also needed for the first time install nowadays

--- Init/Start-Windows.ps1
+++ Init/Start-Windows.ps1
@@ -5,8 +5,14 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# For bootstrap we need just git and this module
+# For bootstrap we need just git, rust, powershell core and this module
 choco install -s=chocolatey git powershell-core -y
+
+# Select 1 for installation through Visual Studio Community edition
+wget https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe -OutFile .\Downloads\rustup-init.exe
+.\Downloads\rustup-init.exe
+
+# First profile invocation will take some time
 $destination = "$home/Documents/Powershell"
 git clone https://github.com/FallenGameR/MyProfile.git $destination
 
