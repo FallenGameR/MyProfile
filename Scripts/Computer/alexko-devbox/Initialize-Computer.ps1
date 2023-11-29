18096878a941dec1dc7f87a068d84af56a52578c
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-11-29 15:45:34 -0800

    some updates for devbox

---
+++ Scripts/Computer/alexko-devbox/Initialize-Computer.ps1
@@ -0,0 +1,13 @@
+Complete-Once env-computer {
+    Set-EnvironmentVariable "AzCompute" "C:\src\Azure-Compute-Move\"
+    Set-EnvironmentVariable "ApGold" "c:\src\Config-AfGold"
+    Set-EnvironmentVariable "PfGold" "c:\src\Config-PfGold"
+    Set-EnvironmentVariable "NTP" "c:\src\ntp\"
+}
+
+Complete-Once junction-one-drive-ms {
+    New-Item -ItemType Junction -Name "D:\OneDrive.MS" -Target $env:OneDriveCommercial -ea Ignore
+    Set-EnvironmentVariable "OneDriveCommercial" "D:\OneDrive.MS"
+}
+
+tm (Split-Path $PSCommandPath -Leaf)
