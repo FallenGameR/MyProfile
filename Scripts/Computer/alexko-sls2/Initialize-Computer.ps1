88914a795aebdc735c45d19233a8f9ab6f117785
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2024-10-22 12:49:55 -0700

    .

--- Scripts/Computer/alexko-sls2/Initialize-Computer.ps1
+++ Scripts/Computer/alexko-sls2/Initialize-Computer.ps1
@@ -1,7 +1,7 @@
 Complete-Once env-computer {
     Set-EnvironmentVariable "mv"                "c:\src\mv\"
     Set-EnvironmentVariable "config"            "c:\src\config\"
-    Set-EnvironmentVariable "investigations"    "c:\src\ntp\investigations\"
+    Set-EnvironmentVariable "investigations"    "c:\src\investigations\"
     Set-EnvironmentVariable "PfGold"            "c:\src\golds\pf\"
 }
 
