4012d6f048f5149b0a9762c83e92d080f558c644
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2024-10-22 12:51:00 -0700

    .

--- Scripts/Computer/alexko-sls2/Initialize-Computer.ps1
+++ Scripts/Computer/alexko-sls2/Initialize-Computer.ps1
@@ -2,7 +2,7 @@ Complete-Once env-computer {
     Set-EnvironmentVariable "mv"                "c:\src\mv\"
     Set-EnvironmentVariable "config"            "c:\src\config\"
     Set-EnvironmentVariable "investigations"    "c:\src\investigations\"
-    Set-EnvironmentVariable "PfGold"            "c:\src\golds\pf\"
+    Set-EnvironmentVariable "PfGold"            "c:\src\pfgold\"
 }
 
 . $PSScriptRoot\..\Initialize-Work.ps1
