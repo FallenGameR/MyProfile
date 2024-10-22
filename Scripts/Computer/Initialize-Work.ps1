470518fe8ff234527b1c42052955be19a5cfd271
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2024-10-22 12:42:42 -0700

    .

---
+++ Scripts/Computer/Initialize-Work.ps1
@@ -0,0 +1,55 @@
+# Tools availability
+$env:Path += ";C:\tools\FcShell"
+$env:Path += ";C:\tools\dcm.explorer"
+$env:Path += ";C:\tools\xts"
+$env:Path += ";C:\tools\drop"
+$env:Path += ";C:\tools\lens"
+$env:Path += ";C:\tools\prorab"
+$env:Path += ";C:\tools\oneAccess"
+$env:Path += ";C:\tools\sd"
+
+# Modules availability
+$env:PSModulePath = "$env:PSModulePath;C:\tools\DriScripts"
+$env:PSModulePath = "C:\tools\JitShell;$env:PSModulePath"
+$env:PSModulePath = "C:\tools\TipNodeServiceAME;$env:PSModulePath"
+
+# Line endings mitigation
+$env:FZF_BINDINGS_GIT_LINE_ENDINGS_MITIGATION =
+    "src/Services/rwf/bootstrap/ComponentBootstrap/Test/TestContent/Rdm/Decom/Inventory.xml;" +
+    "src/Services/NeMo/NetMgr/ConfigPolicyVerifier/ConfigArchiveServiceMain/files/SecretConfigBleu.json;" +
+    "src/Services/NeMo/NetMgr/ConfigPolicyVerifier/ConfigQueryServiceMain/files/SecretConfigBleu.json;" +
+    "src/Services/NeMo/NetMgr/Test/ComponentTest/NDM.Test.Common/Files/TestGraphs/PortChannelPort_DefaultMtu_CheckMtuValue/ControlPlaneGraph.xml;" +
+    "src/Services/NeMo/NetMgr/Test/ComponentTest/NDM.Test.Common/Files/TestGraphs/PortChannelPort_DefaultMtu_CheckMtuValue/DataPlaneGraph.xml;" +
+    "src/Services/NeMo/NetMgr/Test/ComponentTest/NDM.Test.Common/Files/TestGraphs/PortChannelPort_DefaultMtu_CheckMtuValue/DeviceMetadata.xml;" +
+    "src/Services/NeMo/NetMgr/Test/ComponentTest/NDM.Test.Common/Files/TestGraphs/PortChannelPort_DefaultMtu_CheckMtuValue/PhysicalNetworkGraph.xml"
+# Update-GitLineEndingsMitigation
+
+# FZF quick paths
+$env:FZF_QUICK_PATHS =
+    "$env:mv\src\Client\NTP\;" +
+    "$env:mv\src\Client\NTP\scripts\s1-tools\;" +
+    "$env:mv\src\Client\NTP\scripts\modules\DriScripts\;" +
+    "$env:PfGold\data\Autopilot\NtpReferenceClock\Firmware\"
+
+# Reload DriScripts module
+function reload( [switch] $Official )
+{
+    Get-Module DriScripts | Remove-Module
+
+    if( $Official )
+    {
+        Import-Module C:\tools\DriScripts\DriScripts\DriScripts.psd1 -Force
+    }
+    else
+    {
+        Import-Module DriScripts -Force
+    }
+
+    Get-Module DriScripts | select name, version, path
+}
+
+# Stratum1 tools
+function s1( $name ) { & $env:mv\src\Client\NTP\scripts\s1-tools\Initialize-Stratum1.ps1 $name | code - }
+
+#$env:PSModulePath = "C:\src\mv\src\Client\NTP\scripts\modules;$env:PSModulePath"
+#Set-DsSetting -CockpitQueryFolder "c:\tools\DriScripts\CockpitQuery\"
--- Scripts/Computer/alexko-11/Initialize-Computer.ps1
+++ Scripts/Computer/alexko-11/Initialize-Computer.ps1
@@ -1,10 +1,11 @@
 Complete-Once env-computer {
-    Set-EnvironmentVariable "mv" "v:\src\mv\"
-    Set-EnvironmentVariable "ApGold" "v:\src\golds\ap\"
-    Set-EnvironmentVariable "PfGold" "v:\src\golds\pf\"
-    Set-EnvironmentVariable "ConfigGold" "v:\src\config\"
-    Set-EnvironmentVariable "NTP" "v:\src\ntp\"
+    Set-EnvironmentVariable "mv"                "v:\src\mv\"
+    Set-EnvironmentVariable "config"            "v:\src\config\"
+    Set-EnvironmentVariable "investigations"    "v:\src\ntp\"
+    Set-EnvironmentVariable "PfGold"            "v:\src\golds\pf\"
 }
+
+. $PSScriptRoot\..\Initialize-Work.ps1
 
 Complete-Once install-bandwhich {
     mkdir c:\tools\rust\
@@ -34,53 +35,5 @@ Complete-Once junction-one-drive-ms {
     New-Item -ItemType Junction -Name "D:\OneDrive.MS" -Target $env:OneDriveCommercial -ea Ignore
     Set-EnvironmentVariable "OneDriveCommercial" "D:\OneDrive.MS"
 }
-
-$env:Path = "$env:Path;C:\tools\FcShell"
-$env:Path = "$env:Path;C:\tools\dcm.explorer"
-$env:Path = "$env:Path;C:\tools\xts"
-$env:Path = "$env:Path;C:\tools\drop"
-$env:Path = "$env:Path;C:\tools\lens"
-$env:Path = "$env:Path;C:\tools\prorab"
-$env:Path = "$env:Path;C:\tools\oneAccess"
-
-$env:PSModulePath = "$env:NTP\scripts\modules;$env:PSModulePath"
-$env:PSModulePath = "C:\tools\JitShell;$env:PSModulePath"
-$env:PSModulePath = "C:\tools\TipNodeServiceAME;$env:PSModulePath"
-$env:PSModulePath = "$env:PSModulePath;C:\tools\DriScripts"
-
-$env:FZF_BINDINGS_GIT_LINE_ENDINGS_MITIGATION =
-    "src/Services/rwf/bootstrap/ComponentBootstrap/Test/TestContent/Rdm/Decom/Inventory.xml;" +
-    "src/Services/NeMo/NetMgr/ConfigPolicyVerifier/ConfigArchiveServiceMain/files/SecretConfigBleu.json;" +
-    "src/Services/NeMo/NetMgr/ConfigPolicyVerifier/ConfigQueryServiceMain/files/SecretConfigBleu.json;" +
-    "src/Services/NeMo/NetMgr/Test/ComponentTest/NDM.Test.Common/Files/TestGraphs/PortChannelPort_DefaultMtu_CheckMtuValue/ControlPlaneGraph.xml;" +
-    "src/Services/NeMo/NetMgr/Test/ComponentTest/NDM.Test.Common/Files/TestGraphs/PortChannelPort_DefaultMtu_CheckMtuValue/DataPlaneGraph.xml;" +
-    "src/Services/NeMo/NetMgr/Test/ComponentTest/NDM.Test.Common/Files/TestGraphs/PortChannelPort_DefaultMtu_CheckMtuValue/DeviceMetadata.xml;" +
-    "src/Services/NeMo/NetMgr/Test/ComponentTest/NDM.Test.Common/Files/TestGraphs/PortChannelPort_DefaultMtu_CheckMtuValue/PhysicalNetworkGraph.xml"
-
-# Update-GitLineEndingsMitigation
-
-$env:FZF_QUICK_PATHS =
-    "$env:mv\src\Client\NTP\;" +
-    "$env:mv\src\Client\NTP\scripts\s1-tools\;" +
-    "$env:mv\src\Client\NTP\scripts\modules\DriScripts\;" +
-    "$env:PfGold\data\Autopilot\NtpReferenceClock\Firmware\"
-
-function reload( [switch] $Official )
-{
-    Get-Module DriScripts | Remove-Module
-
-    if( $Official )
-    {
-        Import-Module C:\tools\DriScripts\DriScripts\DriScripts.psd1 -Force
-    }
-    else
-    {
-        Import-Module DriScripts -Force
-    }
-
-    Get-Module DriScripts | select name, version, path
-}
-
-function s1( $name ) { & $env:mv\src\Client\NTP\scripts\s1-tools\Initialize-Stratum1.ps1 $name | code - }
 
 tm (Split-Path $PSCommandPath -Leaf)
