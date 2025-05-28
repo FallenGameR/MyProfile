a3e55c7dc9f83be93bdf8e47481869bbfc3e5d29
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2025-05-27 23:23:04 -0700

    more cleanup

--- Scripts/Computer/alexko-sb2/Initialize-Computer.ps1
+++
@@ -1,8 +0,0 @@
-Complete-Once "Computer-specific environment vars" {
-    Set-EnvironmentVariable "AzCompute" "c:\src\mv\"
-    Set-EnvironmentVariable "ApGold" "c:\src\gold\ap\"
-    Set-EnvironmentVariable "PfGold" "c:\src\gold\pf\"
-    Set-EnvironmentVariable "NTP" "C:\src\ntp\"
-}
-
-tm (Split-Path $PSCommandPath -Leaf)
