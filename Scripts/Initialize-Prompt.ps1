8ac4b3c7f6d2233ed9ba88c9c5971be8d58d0162
Aleksandr Kostikov <git config user.email>
2025-08-11 13:42:08 -0700

    .

--- Scripts/Initialize-Prompt.ps1
+++ Scripts/Initialize-Prompt.ps1
@@ -1,13 +1,10 @@
-# Init yazi
-
-function y
-{
-    $tmp = [System.IO.Path]::GetTempFileName()
+# Init yazi - https://yazi-rs.github.io/docs/quick-start
+function y {
+    $tmp = (New-TemporaryFile).FullName
     yazi $args --cwd-file="$tmp"
     $cwd = Get-Content -Path $tmp -Encoding UTF8
-    if( -not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path )
-    {
-        Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
     }
     Remove-Item -Path $tmp
 }
