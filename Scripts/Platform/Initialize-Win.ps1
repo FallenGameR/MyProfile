65d65ef8e9a3dd31d84054de8766f9c6b90fbc9c
Aleksandr Kostikov <alex.kostikov@gmail.com>
2026-07-15 10:36:15 -0700

    btop added

--- Scripts/Platform/Initialize-Win.ps1
+++ Scripts/Platform/Initialize-Win.ps1
@@ -14,6 +14,9 @@ $addToPath =
     "C:\tools\tagger"
 $env:PATH += [io.path]::PathSeparator + (($addToPath | where{ Test-Path $psitem -ea Ignore }) -join [io.path]::PathSeparator)
 
+# Alias setup
+Set-Alias -Name btop -Value btop4win
+
 # Shortcut setup
 Register-Shortcut "Alt+y" "y" "yazi open"
 
